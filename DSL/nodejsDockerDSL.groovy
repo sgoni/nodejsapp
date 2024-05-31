35936c74114f90268de14d2c06fac77a24790775
s.goni <steven.goni@outlook.com>
2024-05-31 08:58:55 -0600

    Update nodejsDockerDSL.groovy
    Test change user

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('phantom1336/nodejsapp')
+            repositoryName('sgoni/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
